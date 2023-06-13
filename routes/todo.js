090e16962b2dd10284a179783083fa0f9cbf1b2b
brijal.kansara <brijal.kansara@trinarybits.in>
2023-06-13 16:59:22 +0530

    [ADD] validations

--- routes/todo.js
+++ routes/todo.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Todo = require("../models/Todo");
+// Validation library
+const { check, validationResult } = require("express-validator");
 
 // Get all the todos
 router.get("/todos", async (req, res) => {
@@ -13,31 +15,42 @@ router.get("/todos", async (req, res) => {
 });
 
 // Create new Todo
-router.post("/add-todo", async (req, res) => {
-  try {
-    const { title, desc, active } = req.body;
-    const newTodo = new Todo({ title, desc, active });
-    await newTodo.save();
-    res.status(201).json(newTodo);
-  } catch (error) {
-    console.log("create todo", error);
+router.post(
+  "/add-todo",
+  [
+    check("title", "Title must be between 2-30 char").isLength({
+      min: 2,
+      max: 30,
+    }),
+    check("desc", "desc must be between 2-100 char").isLength({
+      min: 2,
+      max: 100,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json(errors);
+    }
+    try {
+      const { title, desc, active } = req.body;
+      const newTodo = new Todo({ title, desc, active });
+      await newTodo.save();
+      res.status(201).json(newTodo);
+    } catch (error) {
+      console.log("create todo", error);
+    }
   }
-});
+);
 
 // Edit Todo
 router.put("/edit-todo/:id", async (req, res) => {
   try {
     const { title, desc, active } = req.body;
     const updatedTodo = {};
-    if (title) {
-      updatedTodo.title = title;
-    }
-    if (desc) {
-      updatedTodo.desc = desc;
-    }
-    if (active) {
-      updatedTodo.active = active;
-    }
+    if (title & title.length > 1) updatedTodo.title = title;
+    if (desc) updatedTodo.desc = desc; 
+    if (active) updatedTodo.active = active;
     const todo = await Todo.findByIdAndUpdate(
       req.params.id,
       { $set: updatedTodo },
@@ -53,9 +66,10 @@ router.put("/edit-todo/:id", async (req, res) => {
 router.delete(`/delete-todo/:id`, async (req, res) => {
   try {
     const todo = await Todo.findByIdAndDelete(req.params.id);
-    res.send(todo);
+    if(!todo) res.send("Todo not exist")
+    else res.send(todo);
   } catch (error) {
-    console.log("delete todo", error);
+    console.log("Delete todo", error);
   }
 });
 
